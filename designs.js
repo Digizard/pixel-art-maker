f60b79633fc876da96218fed77b06c7e8080beb5
SittingFox <SittingFox@users.noreply.github.com>
2017-12-23 23:21:32 -0500

    feat: Implement grid and click color filling

--- designs.js
+++ designs.js
@@ -1,10 +1,72 @@
-// Select color input
-// Select size input
+let colorPicker = document.getElementById('color-picker');
+function getSelectedColor() {
+  return colorPicker.value;
+}
 
-// When size is submitted by the user, call makeGrid()
+let grid = document.getElementById('pixel-canvas');
+grid.addEventListener('click', function (event) {
+  let clickedElement = event.target;
+  let cellElementName = 'TD';
 
-function makeGrid() {
+  if (clickedElement.nodeName === cellElementName) {
+    clickedElement.style.backgroundColor = getSelectedColor();
+  }
+});
 
-// Your code goes here!
 
+let sizePicker = document.getElementById('size-picker');
+
+sizePicker.addEventListener('submit', submitSize);
+
+let inputHeight = document.getElementById('input-height'),
+    inputWidth = document.getElementById('input-width');
+
+function getGridHeight() {
+  return inputHeight.value;
+}
+
+function getGridWidth() {
+  return inputWidth.value;
+}
+
+function submitSize(event) {
+  makeGrid(getGridHeight(), getGridWidth());
+  event.preventDefault();
+  return false;
+}
+
+function makeGrid(height, width) {
+  clearTable();
+  createTable(height, width);
+}
+
+function clearTable() {
+  removeAllChildNodes(grid);
+
+  function removeAllChildNodes(element) {
+    while (element.hasChildNodes()) {
+      grid.removeChild(element.lastChild);
+    }
+  }
+}
+
+function createTable(numRows, numCols) {
+  for (let row = 0; row < numRows; row++) {
+    grid.appendChild(createRow(numCols));
+  }
+
+  function createRow(numCols) {
+    let row = document.createElement('tr');
+
+    for (let col = 0; col < numCols; col++) {
+      row.appendChild(createColumn());
+    }
+
+    return row;
+  }
+
+  function createColumn() {
+    let cell = document.createElement('td');
+    return cell;
+  }
 }
