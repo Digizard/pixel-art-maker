6eb6c56e89e66a3d770025b15c9dee0071cdff72
SittingFox <SittingFox@users.noreply.github.com>
2017-12-24 01:08:57 -0500

    refactor: Split designs.js into Data and View

--- designs.js
+++ designs.js
@@ -1,72 +1,99 @@
-let colorPicker = document.getElementById('color-picker');
-function getSelectedColor() {
-  return colorPicker.value;
-}
-
-let grid = document.getElementById('pixel-canvas');
-grid.addEventListener('click', function (event) {
-  let clickedElement = event.target;
-  let cellElementName = 'TD';
-
-  if (clickedElement.nodeName === cellElementName) {
-    clickedElement.style.backgroundColor = getSelectedColor();
+class Data {
+  constructor() {
+    this.colorPicker = document.getElementById('color-picker');
+    this.sizePicker = document.getElementById('size-picker');
+    this.inputHeight = document.getElementById('input-height');
+    this.inputWidth = document.getElementById('input-width');
   }
-});
+
+  getSelectedColor() {
+    return this.colorPicker.value;
+  }
+
+  getGridHeight() {
+    return this.inputHeight.value;
+  }
+
+  getGridWidth() {
+    return this.inputWidth.value;
+  }
+};
+
+let data = new Data();
+
+class View {
+
+  constructor() {
+    this.grid = document.getElementById('pixel-canvas');
+
+    this.initListeners(this);
+  }
+
+  initListeners(self) {
+    self.initGrid(self);
+    self.initSubmit(self);
+    self.initSizePicker(self);
+  }
+
+  initGrid(self) {
+    this.grid.addEventListener('click', function (event) {
+      let clickedElement = event.target;
+      let cellElementName = 'TD';
+
+      if (clickedElement.nodeName === cellElementName) {
+        clickedElement.style.backgroundColor = data.getSelectedColor();
+      }
+    });
+  }
+
+  initSubmit(self) {
+    self.submitSize = (event) => {
+      self.makeGrid(data.getGridHeight(), data.getGridWidth());
+      event.preventDefault();
+      return false;
+    };
+  }
+
+  initSizePicker(self) {
+    data.sizePicker.addEventListener('submit', self.submitSize);
+  }
 
 
-let sizePicker = document.getElementById('size-picker');
+  makeGrid(height, width) {
+    this.clearTable();
+    this.createTable(height, width);
+  }
 
-sizePicker.addEventListener('submit', submitSize);
+  clearTable() {
+    removeAllChildNodes(this.grid);
 
-let inputHeight = document.getElementById('input-height'),
-    inputWidth = document.getElementById('input-width');
-
-function getGridHeight() {
-  return inputHeight.value;
-}
-
-function getGridWidth() {
-  return inputWidth.value;
-}
-
-function submitSize(event) {
-  makeGrid(getGridHeight(), getGridWidth());
-  event.preventDefault();
-  return false;
-}
-
-function makeGrid(height, width) {
-  clearTable();
-  createTable(height, width);
-}
-
-function clearTable() {
-  removeAllChildNodes(grid);
-
-  function removeAllChildNodes(element) {
-    while (element.hasChildNodes()) {
-      grid.removeChild(element.lastChild);
+    function removeAllChildNodes(element) {
+      while (element.hasChildNodes()) {
+        element.removeChild(element.lastChild);
+      }
     }
   }
-}
 
-function createTable(numRows, numCols) {
-  for (let row = 0; row < numRows; row++) {
-    grid.appendChild(createRow(numCols));
-  }
-
-  function createRow(numCols) {
-    let row = document.createElement('tr');
-
-    for (let col = 0; col < numCols; col++) {
-      row.appendChild(createColumn());
+  createTable(numRows, numCols) {
+    for (let row = 0; row < numRows; row++) {
+      this.grid.appendChild(createRow(numCols));
     }
 
-    return row;
-  }
+    function createRow(numCols) {
+      let row = document.createElement('tr');
 
-  function createColumn() {
-    let cell = document.createElement('td');
-    return cell;
+      for (let col = 0; col < numCols; col++) {
+        row.appendChild(createColumn());
+      }
+
+      return row;
+    }
+
+    function createColumn() {
+      let cell = document.createElement('td');
+      return cell;
+    }
   }
-}
+};
+
+let view = new View();
